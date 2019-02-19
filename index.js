c84cfeb0b5c14682ae05290a697e06f8cb31739b
joshua-disney <joshua.a.disney@gmail.com>
2019-02-19 15:36:26 -0700

    Got server up and running

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
+require("dotenv").config();
 
-const server = express();
-
-server.use(express.json());
-server.use(helmet());
+const server = require("./server.js");
 
 // endpoints here
 
-const port = 3300;
-server.listen(port, function() {
+const port = process.env.PORT || 3300;
+server.listen(port, () => {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
